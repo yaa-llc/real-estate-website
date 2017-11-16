7a6be66e988da47719191811038db01cf3f86619
Yaa Otchere <yaa@yaaotchere.ca>
2017-11-16 07:48:10 -0500

    making collection pgs

---
+++ page-stone-collection.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Template Name: Home Page
+ *
+ * @package Pacific_Art_Stone
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+			<?php
+			while ( have_posts() ) : the_post();
+
+
+				get_template_part( 'template-parts/flexible-home', 'page' );
+
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+
+			endwhile; // End of the loop.
+			?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
---
+++ template-parts/stone-collection/collection-brochure.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yaaotchere
+ * Date: 2017-11-15
+ * Time: 10:08 AM
+ */
