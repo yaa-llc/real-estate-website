930831c0fb78e4eac7604ad61e51bcad2610c0f2
Yaa Otchere <yaa@yaaotchere.ca>
2017-11-22 11:30:26 -0500

    aking the quick links sections

--- template-parts/home/quick-links.php
+++ template-parts/home/quick-links.php
@@ -1,8 +1,26 @@
+<section class="quick-links-section">
+    <div class="quick-links-wrapper">
+        <?php if ( have_rows( 'quick_link' ) ) : ?>
+            <?php while ( have_rows( 'quick_link' ) ) : the_row(); ?>
+                <?php $download_a_brochure = get_sub_field( 'download_a_brochure' ); ?>
+                <?php if ( $download_a_brochure ) { ?>
+                    <?php $brochure_image = wp_get_attachment_image('389', 'full'); ?>
 
-<?php if ( have_rows( 'quick_link' ) ) : ?>
-    <?php while ( have_rows( 'quick_link' ) ) : the_row(); ?>
-        <?php the_sub_field( 'link' ); ?>
-    <?php endwhile; ?>
-<?php else : ?>
-    <?php // no rows found ?>
-<?php endif; ?>
+                    <div class="quick-links">
+                        <?php echo $brochure_image; ?>
+                        <a href="<?php echo $download_a_brochure['url']; ?>" class="block-link">Download A Brochure</a>
+                    </div>
+
+                    <div class="quick-links">
+                        <!-- make dealer image varibale -->
+                        <?php $dealer_image = wp_get_attachment_image('390', 'full'); ?>
+                        <?php echo $dealer_image; ?>
+                        <a href="<?php the_sub_field( 'find_a_dealer' ); ?>" class="block-link">Find a Dealer</a>
+                    </div>
+
+                <?php } ?>
+
+            <?php endwhile; ?>
+        <?php endif; ?>
+    </div>
+</section>
