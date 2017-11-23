ed220061635fad00ff005b9509bf668af22775f8
Yaa Otchere <yaa@yaaotchere.ca>
2017-11-23 07:48:15 -0500

    making block links

--- template-parts/home/stone-collection.php
+++ template-parts/home/stone-collection.php
@@ -1,11 +1,12 @@
 <!-- getting the variable for background image -->
 
-<?php $stone_collection_featured_image = get_sub_field( 'stone_collection_featured_image' ); ?>
+<?php $stone_collection_background_image = get_sub_field( 'stone_collection_row_background_image', 'full' ); ?>
 
-<section class="featured-collection">
+<section class="featured-collection" style="background:url('<?php echo $stone_collection_background_image['url']; ?>')">
     <div class="inner-wrapper">
         <div class="feature-wrapper">
-            <div class="featured-text-wrapper">
+            <!-- adding a class of half to see if I can make this easier for media queries. ie) at phone half = 75% -->
+            <div class="featured-text-wrapper half">
                 <h4><?php the_sub_field( 'section_title' ); ?></h4>
                 <h2><?php $post_object = get_sub_field( 'stone_collection' ); ?></h2>
                 <p><?php the_sub_field( 'stone_collection_description' ); ?></p>
@@ -17,7 +18,7 @@
                 <?php endif; ?>
             </div>
 
-            <div class="featured-image-wrapper">
+            <div class="featured-image-wrapper half">
                 <?php $stone_collection_featured_image = get_sub_field( 'stone_collection_featured_image' ); ?>
                 <?php if ( $stone_collection_featured_image ) { ?>
                 <img src="<?php echo $stone_collection_featured_image['url']; ?>" alt="<?php echo $stone_collection_featured_image['alt']; ?>" />
